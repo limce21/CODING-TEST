1c219c609e4ae38c9c556256852a46b9caf2dae4
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-03-18 22:12:47 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,18 +1,15 @@
 import sys
 
 n, k = map(int, sys.stdin.readline().rstrip().split())
-
 data = []
-
-for _ in range(n):
+for i in range(n):
   data.append(int(sys.stdin.readline().rstrip()))
 
 data.sort(reverse=True)
 
 result = 0
-
-for i in data:
-  result += k // i
-  k %= i
-
+for money in data:
+  if k >= money:
+    result += k // money
+    k %= money
 print(result)
