96c51eb228b27cac2d0e732ef3215bbe14b4644b
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-03-28 11:26:29 +0900

    [Silver I] Title: RGB거리, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+data = []
+for _ in range(n):
+  data.append(list(map(int, input().split())))
+
+for i in range(1, n):
+  data[i][0] += min(data[i - 1][1], data[i - 1][2])
+  data[i][1] += min(data[i - 1][0], data[i - 1][2])
+  data[i][2] += min(data[i - 1][0], data[i - 1][1])
+
+print(min(data[-1]))
