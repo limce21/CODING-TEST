1f44e7108c0788777cf2bf4ee558c0c715004a3f
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-03-29 22:34:15 +0900

    [Silver III] Title: 통계학, Time: 492 ms, Memory: 56548 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -2,35 +2,22 @@ import sys
 from collections import Counter
 
 n = int(sys.stdin.readline().rstrip())
-
 data = []
+
 for _ in range(n):
   data.append(int(sys.stdin.readline().rstrip()))
 
-result = round(sum(data) / n)
-print(result)
+print(round(sum(data) / n))
 
-data.sort()
-result = data[n // 2]
-print(result)
+data_sort = sorted(data)
+print(data_sort[n // 2])
 
 counter = Counter(data)
-dict_counter = dict(counter)
-max_count = max(dict_counter, key=dict_counter.get)
-count_list = []
-for i in data:
-  if dict_counter[i] == dict_counter[max_count]:
-    count_list.append(i)
-count_set = set(count_list)
-count_list = list(count_set)
-count_list.sort()
-if len(count_list) > 1:
-  result = count_list[1]
+counter = list(counter.items())
+counter.sort(key=lambda x: (-x[1], x[0]))
+if len(counter) >= 2 and counter[0][1] == counter[1][1]:
+  print(counter[1][0])
 else:
-  result = count_list[0]
-print(result)
+  print(counter[0][0])
 
-max_value = max(data)
-min_value = min(data)
-result = max_value - min_value
-print(result)
+print(max(data) - min(data))
