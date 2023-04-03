d808ed9cce66a65923e7a29ea5419dd47b14607c
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-04-03 22:24:11 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 320 ms, Memory: 50276 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,28 +1,30 @@
 import sys
+from collections import deque
 
-sys.setrecursionlimit(10**6)
 
-
-def dfs(graph, v, visited):
-  global dict_data
-  visited[v] = True
-  for i in graph[v]:
-    if not visited[i]:
-      dict_data[i] = v
-      dfs(graph, i, visited)
+def bfs():
+  queue = deque()
+  queue.append(1)
+  visited[1] = True
+  while queue:
+    value = queue.popleft()
+    for i in graph[value]:
+      if not visited[i]:
+        data[i] = value
+        queue.append(i)
+        visited[i] = True
 
 
 n = int(sys.stdin.readline().rstrip())
 graph = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
+data = [0] * (n + 1)
+
 for _ in range(n - 1):
   a, b = map(int, sys.stdin.readline().rstrip().split())
   graph[a].append(b)
   graph[b].append(a)
 
-dict_data = dict()
-dfs(graph, 1, visited)
-data = sorted(dict_data.items(), key=lambda x: x[0])
-
-for i in data:
-  print(i[1])
+visited = [False] * (n + 1)
+bfs()
+for i in range(2, n + 1):
+  print(data[i])
