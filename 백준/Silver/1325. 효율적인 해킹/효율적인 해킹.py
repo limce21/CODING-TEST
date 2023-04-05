5f7e9a1523ea248fdd58e60f114cdea7db37ac01
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-04-05 13:57:04 +0900

    [Silver I] Title: 효율적인 해킹, Time: 11396 ms, Memory: 215288 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,37 +1,41 @@
 import sys
 from collections import deque
 
+input = sys.stdin.readline
 
-def bfs(graph, start, visited):
-  queue = deque([start])
+
+def bfs(start):
+  queue = deque([])
+  queue.append(start)
   visited[start] = True
-  count = 0
+  result = 1
   while queue:
-    count += 1
-    v = queue.popleft()
-    for i in graph[v]:
+    value = queue.popleft()
+    for i in graph[value]:
       if not visited[i]:
         visited[i] = True
         queue.append(i)
-  return count
+        result += 1
+  return result
 
 
-n, m = map(int, sys.stdin.readline().rstrip().split())
-
+n, m = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
+
 for _ in range(m):
-  a, b = map(int, sys.stdin.readline().rstrip().split())
+  a, b = map(int, input().split())
   graph[b].append(a)
 
-data = []
+answer = []
 for i in range(1, n + 1):
   visited = [False] * (n + 1)
-  data.append((i, bfs(graph, i, visited) - 1))
+  answer.append((i, bfs(i)))
 
-data = sorted(data, key=lambda x: x[1], reverse=True)
-max_value = data[0][1]
-for i in data:
+answer.sort(key=lambda x: (-x[1], x[0]))
+max_value = answer[0][1]
+
+for i in answer:
   if i[1] == max_value:
-    print(i[0], end=' ')
+    print(i[0], end=" ")
   else:
     break
