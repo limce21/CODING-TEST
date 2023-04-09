b2fbf393e0ef86dc6860da5b0be0908be2cdcf57
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-04-09 12:44:44 +0900

    [Silver II] Title: 한 줄로 서기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -1,20 +1,9 @@
 import sys
-from itertools import permutations
 
 n = int(sys.stdin.readline().rstrip())
-data = list(map(int, sys.stdin.readline().rstrip().split()))
-num = [i + 1 for i in range(n)]
-num = permutations(num, n)
-
-for i in num:
-  temp = []
-  for j in range(1, n + 1):
-    cnt = 0
-    for k in range(n):
-      if i[k] == j:
-        temp.append(cnt)
-        break
-      if i[k] > j:
-        cnt += 1
-  if temp == data:
-    print(' '.join(map(str,i)))
+data = list(map(int, sys.stdin.readline().rstrip().split()))[::-1]
+result = []
+for i in data:
+  result.insert(i, n)
+  n -= 1
+print(*result)
