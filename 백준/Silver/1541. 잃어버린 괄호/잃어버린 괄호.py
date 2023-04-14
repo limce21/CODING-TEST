2b74b4f7fda815876eacfaac90d23d6a0ed0f506
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-04-14 11:47:32 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,11 @@
+import sys
+
+expression = sys.stdin.readline().rstrip().split('-')
+result = 0
+for i in expression[0].split('+'):
+  result += int(i)
+for i in expression[1:]:
+  for j in i.split('+'):
+    result -= int(j)
+
+print(result)
