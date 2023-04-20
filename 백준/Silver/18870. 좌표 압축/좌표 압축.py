a16a74b4b283da1861ff38ac4aaf3372e5842f87
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-04-21 08:47:28 +0900

    [Silver II] Title: 좌표 압축, Time: 2732 ms, Memory: 153216 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,19 +1,11 @@
 import sys
 from bisect import bisect_left
 
-def count_by_range(a, left_value):
-  left_index = bisect_left(a, left_value)
-  return left_index
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline().rstrip())
-data = list(map(int, sys.stdin.readline().rstrip().split()))
-data_set = set(data)
-sorted_data = sorted(list(data_set))
+n = int(input())
+x_list = list(map(int, input().split()))
+sorted_x_list = sorted(list(set(x_list)))
 
-result = []
-for x in data:
-  count = count_by_range(sorted_data, x)
-  result.append(count)
-
-for i in result:
-  print(i, end=' ')
+for x in x_list:
+  print(bisect_left(sorted_x_list, x), end=" ")
