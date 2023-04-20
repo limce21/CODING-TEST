e7bbcfb784128a699402ce2645ea97bc7fb88da1
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-04-20 11:55:19 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,25 +1,13 @@
 import sys
 
+dp = [[] for _ in range(41)]
+dp[0] = (1, 0)
+dp[1] = (0, 1)
 
-def fibo(x):
-  if x == 0:
-    return 1
-  if x == 1:
-    return 1
-  if dp[x] != 0:
-    return dp[x]
-  dp[x] = fibo(x - 1) + fibo(x - 2)
-  count[x] = (count[x-1][0] + count[x-2][0], count[x-1][1] + count[x-2][1])
-  return dp[x]
+for i in range(2, 41):
+  dp[i] = (dp[i - 1][0] + dp[i - 2][0], dp[i - 1][1] + dp[i - 2][1])
 
-t = int(sys.stdin.readline().rstrip())
-count = [(0,0) for _ in range(41)]
-count[0] = (1,0)
-count[1] = (0,1)
-dp = [0] * 41
-
+t = int(sys.stdin.readline())
 for _ in range(t):
-  n = int(sys.stdin.readline().rstrip())
-  fibo(n)
-  zero, one = count[n]
-  print(zero, one)
+  n = int(sys.stdin.readline())
+  print(*dp[n])
