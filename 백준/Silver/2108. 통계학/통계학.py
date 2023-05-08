c420edc268cf1001fd4042196dcaecb90b96e6dc
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-05-08 11:53:41 +0900

    [Silver III] Title: 통계학, Time: 464 ms, Memory: 56700 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,21 +1,20 @@
 import sys
 from collections import Counter
 
-n = int(sys.stdin.readline().rstrip())
+input = sys.stdin.readline
+n = int(input())
 data = []
 
 for _ in range(n):
-  data.append(int(sys.stdin.readline().rstrip()))
+  data.append(int(input()))
 
-print(round(sum(data) / n))
-
-data_sort = sorted(data)
-print(data_sort[n // 2])
-
-counter = Counter(data)
-counter = list(counter.items())
+sorted_data = sorted(data)
+counter = list(Counter(data).items())
 counter.sort(key=lambda x: (-x[1], x[0]))
-if len(counter) >= 2 and counter[0][1] == counter[1][1]:
+
+print(round(sum(data)/n))
+print(sorted_data[n//2])
+if n >= 2 and counter[0][1] == counter[1][1]:
   print(counter[1][0])
 else:
   print(counter[0][0])
