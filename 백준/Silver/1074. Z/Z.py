4ea52eb421679cffafd94506f10566d70c55f00a
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-05-09 23:05:42 +0900

    [Silver I] Title: Z, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,34 +1,28 @@
 import sys
 
-input = sys.stdin.readline
 
-
-def solution(x, y, now):
-  global result, r, c, answer
-  if now == 2:
-    for i in range(x, x + 2):
-      for j in range(y, y + 2):
-        if i == r and j == c:
-          answer = result
-          return
-        else:
-          result += 1
-  dx = [0, 0, now // 2, now // 2]
-  dy = [0, now // 2, 0, now // 2]
-  for i in range(4):
-    nx = x + dx[i]
-    ny = y + dy[i]
-    if nx <= r < nx + now // 2 and ny <= c < ny + now // 2:
-      solution(nx, ny, now // 2)
+def z(r, c, n, value):
+  if n == 2:
+    if r == 0 and c == 0:
+      print(value)
+    elif r == 0 and c == 1:
+      print(value+1)
+    elif r == 1 and c == 0:
+      print(value+2)
     else:
-      result += (now // 2)**2
-  return
+      print(value+3)
+    return
+  if r < n // 2 and c < n // 2:
+    z(r % (n // 2), c % (n // 2), n // 2, value)
+  elif r < n // 2 and c >= n // 2:
+    z(r % (n // 2), c % (n // 2), n // 2, value + (n // 2)**2)
+  elif r >= n // 2 and c < n // 2:
+    z(r % (n // 2), c % (n // 2), n // 2, value + 2 * ((n // 2)**2))
+  else:
+    z(r % (n // 2), c % (n // 2), n // 2, value + 3 * ((n // 2)**2))
 
 
-n, r, c = map(int, input().split())
-N = 2**n
+n, r, c = map(int, sys.stdin.readline().split())
+n = 2**n
 
-result = 0
-answer = 0
-solution(0, 0, N)
-print(answer)
+z(r, c, n, 0)
