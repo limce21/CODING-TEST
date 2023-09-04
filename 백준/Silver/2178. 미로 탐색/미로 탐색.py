375a0f19d087adf15d4a7d358209c3c6326ba645
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-09-04 16:24:48 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,7 +3,7 @@ from collections import deque
 
 
 def bfs(x, y):
-  queue = deque([])
+  queue = deque()
   queue.append((x, y))
   visited[x][y] = True
   while queue:
@@ -11,26 +11,20 @@ def bfs(x, y):
     for i in range(4):
       nx = vx + dx[i]
       ny = vy + dy[i]
-      if nx < 0 or nx >= n or ny < 0 or ny >= m:
-        continue
-      if graph[nx][ny] == 0:
-        continue
-      if not visited[nx][ny]:
+      if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and graph[nx][ny] != 0:
+        queue.append((nx, ny))
         visited[nx][ny] = True
         graph[nx][ny] = graph[vx][vy] + 1
-        queue.append((nx, ny))
-
-  return graph[n - 1][m - 1]
 
 
-n, m = map(int, sys.stdin.readline().rstrip().split())
-graph = []
-visited = [[False] * m for _ in range(n)]
+n, m = map(int, sys.stdin.readline().split())
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
-
+visited = [[False] * m for _ in range(n)]
+graph = []
 for _ in range(n):
   arr = list(sys.stdin.readline().rstrip())
   graph.append(list(map(int, arr)))
 
-print(bfs(0, 0))
+bfs(0, 0)
+print(graph[n - 1][m - 1])
