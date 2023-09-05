6fecd4e7e7b610889b21dfea35b55761094469e0
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-09-05 16:14:41 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 684 ms, Memory: 65120 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,30 +1,32 @@
 import sys
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
+from collections import deque
 
 
-def dfs(start):
+def bfs(graph, start, visited):
+  global result
+  queue = deque([start])
   visited[start] = True
-  for i in graph[start]:
-    if not visited[i]:
-      visited[i] = True
-      dfs(i)
-  return True
+  while queue:
+    v = queue.popleft()
+    for i in graph[v]:
+      if not visited[i]:
+        visited[i] = True
+        queue.append(i)
+  result += 1
 
 
-n, m = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
+n, m = map(int, sys.stdin.readline().rstrip().split())
 visited = [False] * (n + 1)
+graph = [[] for _ in range(n + 1)]
+result = 0
 
-for i in range(m):
-  a, b = map(int, input().split())
-  graph[a].append(b)
-  graph[b].append(a)
+for _ in range(m):
+  u, v = map(int, sys.stdin.readline().rstrip().split())
+  graph[u].append(v)
+  graph[v].append(u)
 
-cnt = 0
 for i in range(1, n + 1):
   if not visited[i]:
-    dfs(i)
-    cnt += 1
+    bfs(graph, i, visited)
 
-print(cnt)
+print(result)
