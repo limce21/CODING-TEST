a3b3f2bca6cd6df13fdf2e91e4210036d6066033
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-10-07 12:32:09 +0900

    [Gold IV] Title: 빙산, Time: 896 ms, Memory: 215732 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,70 +1,71 @@
 import sys
+from collections import deque 
 
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 
-def dfs(x, y, visited):
+def bfs(x, y, visited):
+  queue = deque([])
+  queue.append((x, y))
   visited[x][y] = True
-  for i in range(4):
-    nx = x + d[i][0]
-    ny = y + d[i][1]
-    if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] != 0 and not visited[nx][
-        ny]:
-      visited[nx][ny] = True
-      dfs(nx, ny, visited)
+  while queue:
+    vx, vy = queue.popleft()
+    for i in range(4):
+      nx = vx + d[i][0]
+      ny = vy + d[i][1]
+      if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] != 0 and not visited[nx][
+          ny]:
+        visited[nx][ny] = True
+        queue.append((nx, ny))
 
 
 def melt():
   cnt = 0
-  while True:
+  while icebergs:
     cnt += 1
-    minus_lst = [0] * len(icebergs)
-    zero_cnt = 0
-    for i in range(len(icebergs)):
-      x, y = icebergs[i]
-      if graph[x][y] == 0:
-        zero_cnt += 1
-        continue
+    minus_lst = dict.fromkeys(icebergs, 0)
+    for x, y in icebergs:
       for j in range(4):
         nx = x + d[j][0]
         ny = y + d[j][1]
         if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-          minus_lst[i] += 1
-    if zero_cnt == len(icebergs):
-      print(0)
-      exit()
+          minus_lst[(x, y)] += 1
 
-    for i in range(len(icebergs)):
-      x, y = icebergs[i]
-      if graph[x][y] == 0:
-        continue
-      graph[x][y] -= minus_lst[i]
-      if graph[x][y] < 0:
-        graph[x][y] = 0
+    remove_lst = []
+    for x, y in icebergs:
+      graph[x][y] -= minus_lst[(x, y)]
+      if graph[x][y] <= 0:
+        remove_lst.append((x, y))
+
+    for x, y in remove_lst:
+      graph[x][y] = 0
+      icebergs.remove((x, y))
 
     visited = [[False] * m for _ in range(n)]
     result = 0
-    for i in range(n):
-      for j in range(m):
-        if not visited[i][j] and graph[i][j] != 0:
-          dfs(i, j, visited)
-          result += 1
+
+    for x, y in icebergs:
+      if not visited[x][y] and graph[x][y] > 0:
+        bfs(x, y, visited)
+        result += 1
+
     if result >= 2:
       print(cnt)
       exit()
+
+  print(0)
 
 
 n, m = map(int, input().split())
 d = [(0, -1), (0, 1), (-1, 0), (1, 0)]
 graph = []
-icebergs = []
+icebergs = set()
 
 for i in range(n):
   arr = list(map(int, input().split()))
   for j in range(m):
     if arr[j] != 0:
-      icebergs.append((i, j))
+      icebergs.add((i, j))
   graph.append(arr)
 
 melt()
