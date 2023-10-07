4915a9f8615fc956e03bfae16b8cd4b8026c5901
ChaenLim <76549753+limce21@users.noreply.github.com>
2023-10-07 23:27:44 +0900

    [Gold V] Title: 동전 1, Time: 204 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = []
+dp = [0] * (k + 1)
+for _ in range(n):
+  coins.append(int(input()))
+
+dp[0] = 1
+for coin in coins:
+  for i in range(coin, k + 1):
+    dp[i] = dp[i] + dp[i - coin]
+
+print(dp[k])
