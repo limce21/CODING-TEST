519b304976b34acd8098187bd9bb3f2fe12970c7
ChaenLim <76549753+limce21@users.noreply.github.com>
2024-12-12 21:13:31 +0900

    [Gold V] Title: 퇴사 2, Time: 660 ms, Memory: 310172 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int[] T = new int[N+1];
+    int[] P = new int[N+1];
+
+    for (int i = 1; i < N+1; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      T[i] = Integer.parseInt(st.nextToken());
+      P[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int[] dp = new int[N + 2];
+    for (int i = 1; i <= N; i++) {
+      if (i + T[i] <= N+1) {
+        dp[i + T[i]] = Math.max(dp[i + T[i]], dp[i] + P[i]);
+      }
+      dp[i+1] = Math.max(dp[i], dp[i + 1]);
+    }
+    System.out.println(dp[N+1]);
+  }
+}
+
