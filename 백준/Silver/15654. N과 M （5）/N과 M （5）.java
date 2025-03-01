3dba153a01460c34248df8f605e23e2e7fb9c44e
ChaenLim <76549753+limce21@users.noreply.github.com>
2025-03-01 15:27:17 +0900

    [Silver III] Title: N과 M (5), Time: 368 ms, Memory: 42492 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static BufferedReader br;
+  static StringTokenizer st;
+  static StringBuilder sb = new StringBuilder();
+  static int n;
+  static int m;
+  static int[] nArr;
+  static int[] arr = new int[9];
+  static boolean[] isUsed = new boolean[10001];
+
+  static void func(int k) {
+    if (k == m) {
+      for (int i = 0; i < m; i++) {
+        sb.append(arr[i] + " ");
+      }
+      sb.append("\n");
+      return;
+    }
+    for (int i = 0; i < n; i++) {
+      int val = nArr[i];
+      if (!isUsed[val]) {
+        isUsed[val] = true;
+        arr[k] = val;
+        func(k + 1);
+        isUsed[val] = false;
+      }
+    }
+  }
+
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+
+    st = new StringTokenizer(br.readLine());
+    nArr = new int[n];
+    for (int i = 0; i < n; i++) {
+      nArr[i] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(nArr);
+    
+    func(0);
+    System.out.println(sb);
+  }
+}
