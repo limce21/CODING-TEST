ddbcd242f10c5dc7d0d020daa035cdcfe0b7528b
ChaenLim <76549753+limce21@users.noreply.github.com>
2025-03-02 14:44:52 +0900

    [Gold V] Title: 감소하는 수, Time: 124 ms, Memory: 14948 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static Queue<long[]> queue = new LinkedList<>();
+  static LinkedList<Long> arr = new LinkedList<Long>();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    for (int i = 0; i < 10; i++) {
+      arr.add((long)i);
+      queue.offer(new long[] { i, i });
+    }
+    
+    while (!queue.isEmpty()) {
+      long[] cur = queue.poll();
+      long idx = cur[0];
+      long last = cur[1];
+      for (int i = 0; i < last; i++) {
+        String val = Long.toString(idx) + Long.toString(i);
+        queue.offer(new long[] { Long.parseLong(val), i });
+        arr.add(Long.parseLong(val));
+      }
+    }
+    
+    if (n >= arr.size()) {
+      System.out.println(-1);
+    } else {
+      System.out.println(arr.get(n));
+    }
+  }
+}
