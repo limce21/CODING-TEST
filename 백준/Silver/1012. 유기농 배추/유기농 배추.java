de363907e28b90f5a23fed9be7be34a425cba9d5
ChaenLim <76549753+limce21@users.noreply.github.com>
2025-10-01 22:38:28 +0900

    [Silver II] Title: 유기농 배추, Time: 140 ms, Memory: 16044 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -7,30 +7,29 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-  static int m;
   static int n;
+  static int m;
   static int[][] graph;
   static boolean[][] visited;
+  static int[] dx = { 0, 0, -1, 1 };
+  static int[] dy = { -1, 1, 0, 0 };
 
   static void bfs(int x, int y) {
     Queue<int[]> queue = new LinkedList<>();
-    int[] dx = { -1, 1, 0, 0 };
-    int[] dy = { 0, 0, -1, 1 };
-    visited[x][y] = true;
     queue.offer(new int[] { x, y });
+    visited[x][y] = true;
 
     while (!queue.isEmpty()) {
-      int[] curr = queue.poll();
+      int[] v = queue.poll();
       for (int i = 0; i < 4; i++) {
-        int nx = curr[0] + dx[i];
-        int ny = curr[1] + dy[i];
+        int nx = v[0] + dx[i];
+        int ny = v[1] + dy[i];
         if (nx < 0 || nx >= n || ny < 0 || ny >= m)
           continue;
         if (graph[nx][ny] == 0 || visited[nx][ny])
           continue;
         visited[nx][ny] = true;
-        queue.offer(new int[] { nx, ny });
+        queue.offer(new int[] {nx, ny});
       }
     }
   }
@@ -38,13 +37,11 @@ public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int t = Integer.parseInt(br.readLine());
-
     for (int c = 0; c < t; c++) {
       StringTokenizer st = new StringTokenizer(br.readLine());
       m = Integer.parseInt(st.nextToken());
       n = Integer.parseInt(st.nextToken());
       int k = Integer.parseInt(st.nextToken());
-
       graph = new int[n][m];
       visited = new boolean[n][m];
       for (int i = 0; i < k; i++) {
@@ -54,16 +51,17 @@ public class Main {
         graph[y][x] = 1;
       }
 
-      int result = 0;
+      int answer = 0;
       for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-          if (graph[i][j] == 1 && !visited[i][j]) {
+          if (graph[i][j] ==1 && !visited[i][j]) {
             bfs(i, j);
-            result++;
+            answer++;
           }
         }
       }
-      System.out.println(result);
+      System.out.println(answer);
     }
+
   }
 }
