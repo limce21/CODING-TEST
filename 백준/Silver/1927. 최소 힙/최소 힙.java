285b965cbe408ada2cbe3049d2723035d5e6497d
ChaenLim <76549753+limce21@users.noreply.github.com>
2025-10-31 22:45:59 +0900

    [Silver II] Title: 최소 힙, Time: 832 ms, Memory: 37192 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+    int n = Integer.parseInt(st.nextToken());
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      if (x == 0) {
+        if (!pq.isEmpty()) {
+          System.out.println(pq.poll());
+        } else {
+          System.out.println(0);
+        }
+        
+      } else {
+        pq.offer(x);
+      }
+    }
+  }
+}
